293b0344cb117e8a84f2f3c3b16b77e2e22b7a23
Udit Shah <uditshah15@gmail.com>
2025-08-05 08:40:34 +1200

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,18 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Header() {
+
   return (
     <header className="flex justify-center py-8">
-      <Image 
-        src="/jelly-belly-logo.webp" 
-        alt="Jelly Belly Logo"
-        width={160}
-        height={160}
-      />
+      <Link href="/">
+        <Image 
+            src="/jelly-belly-logo.webp" 
+            alt="Jelly Belly Logo"
+            width={160}
+            height={160}
+          />
+      </Link>
     </header>
   );
 } 
