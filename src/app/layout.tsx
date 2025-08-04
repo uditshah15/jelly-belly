e159dab6cff85a787a310c9b35ef1ffe266fb56a
Udit Shah <uditshah15@gmail.com>
2025-08-05 08:40:54 +1200

    Update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Header, Footer } from "@/components";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-purple-50`}
       >
-        {children}
+        <Header />
+          {children}
+        <Footer />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
 'use client';
 
-import { Header, Footer } from '@/components';
 import { JellyBeanList } from '@/features';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-purple-50">
-      <Header />
- 
+    <div className="min-h-screen"> 
       <main className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <JellyBeanList />
       </main>
- 
-      <Footer />
     </div>
   );
 }
